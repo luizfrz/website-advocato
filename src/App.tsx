6ae8fb623b0052c26a5e586182d131e5ef9e8844
luizfrz <luizfranca2005@gmail.com>
2026-06-17 13:04:59 -0300

    style: adjust wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { AiOutlineFileProtect } from "react-icons/ai";
 import { MdVerified, MdVerifiedUser } from "react-icons/md";
-import { BsArrowCounterclockwise } from "react-icons/bs";
+import { GiInjustice } from "react-icons/gi";
+import { FaRegTrashAlt } from "react-icons/fa";
 import { FaComments } from "react-icons/fa";
-import "./App.css";
+import "../public/style/App.css";
 
 function App() {
     const [Process, setProcess] = useState("");
@@ -22,12 +23,10 @@ function App() {
                 document.removeEventListener("keydown", handleEsc);
             };
 
-
         }, []);
 
         const handleConsultar = () => {
             if (Process.length < 20) return alert("Numero de processo invalido");
-
             setShowModal(true);
         };
         
@@ -37,7 +36,7 @@ function App() {
         };
 
     return (
-        <div className="Consult">
+        <div className="consultProcess">
             <h1>Advocato</h1>
             <h2>Consulta de processos de forma simples</h2>
             <input
@@ -48,12 +47,13 @@ function App() {
                 placeholder="Insira o número do processo"
                 onChange={(e) => setProcess(e.target.value)}
             />
-            <button onClick={handleConsultar}>Consultar</button> <button  className="Resetprocess" onClick={hadleRepetConsultar}><BsArrowCounterclockwise/></button>
+            <button onClick={handleConsultar}>Consultar</button> <button  className="resetProcess" onClick={hadleRepetConsultar}><FaRegTrashAlt/></button>
+            
             <br />
             <p>Consulte seus processos de forma clara e rápida</p>
-            
+            <br />
             <div className="info">
-                <h2 className="info-title"><AiOutlineFileProtect/> Como funciona</h2> 
+                <h2 className="infoTitle"><AiOutlineFileProtect/> Como funciona</h2> 
                 <ol>
                    <li> <strong>1</strong> Digite o numero do processo
                    </li>
@@ -64,8 +64,8 @@ function App() {
                    </li>
                 </ol>
             </div>
-            <div className="features">
-                <p> <MdVerifiedUser/>Dados Publicos</p><p>Sem cadastro</p> <p>Gratuito</p>
+            <div className="featureProcess">
+                <p> <MdVerifiedUser/>Dados Publicos</p> <p>Sem cadastro</p> <p>Gratuito</p>
             </div>
 
             {showModal && (
@@ -73,28 +73,39 @@ function App() {
                     <div  onClick={(e) => e.stopPropagation()}>
                         <div className="Resumeprocess">
                             <details>
-                                <summary><FaComments/>Resumo simplificado</summary>
-                                <ul>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
+                                <summary ><FaComments/>Resumo simplificado</summary>
+                                <ul >
+                                    <div className="summaryProcess">
+                                    <li>lopsum</li>
+                                    <li>Pagamento de multa</li>
+                                    <li>Prestacao de servico comuntario</li>
+                                    </div>
                                 </ul>
                                 <p></p>
                             </details>
                         </div>
-                        <div className="Resumeprocess">
+                        <div className="resumeProcess">
                             <details>
                                 <summary><FaComments/>Informação técnicas</summary>
                                 <ul>
-
+                                    <div className="summaryProcess">
+                                        <li>10 meses e 26 dias de detencao</li>
+                                        <br />
+                                        <li>Pagamento de multa</li>
+                                        <br />
+                                        <li>Prestacao de servico comuntario</li>
+                                    </div>
                                 </ul>
                                 <p></p>
                             </details>
-                        </div>
-                    </div>
-                    <br />
+                            </div>
+                      </div>
                 </div>
             )}
+           <div>
+             <br />
+              <span> <a href="https://www.cnj.jus.br/">CONSELHO NACIONAL DA JS<GiInjustice/>  </a></span>
+          </div>
         </div>
     );
 }
